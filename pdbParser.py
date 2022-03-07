dd28701a8d1ccef5a5beb6a599029d6fdf6dfd0f
JayPip <jakub.pipien@gmail.com>
2022-03-07 14:02:27 +0100

    smiles parser

--- pdbParser.py
+++ pdbParser.py
@@ -1,11 +1,19 @@
 from Bio.PDB import *
+import autode as ade
 
-
-def getStructure(name, filename):
+def getStructureFromPDB(name, filename):
     parser = PDBParser()
     structure = parser.get_structure(name, filename)    
     for atom in structure.get_atoms():
         print(atom)
 
-getStructure("glukoza", "glucose.pdb")
+def getStructureFromSmiles(filename):
+    f = open(filename, "r")
+    smiles = f.readline()
+    f.close()
+    SParser = ade.Parser()
+    SParser.parse(smiles)
+
+getStructureFromSmiles("glucose.smi")
+getStructureFromPDB("glukoza", "glucose.pdb")
 
